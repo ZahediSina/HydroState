36410ccec1690dc45b30a21f5dfe03f63cffd44d
ZahediSina <102585083+ZahediSina@users.noreply.github.com>
2025-11-26 15:12:53 +1100

    get AIC bugs fiixed for NHMM

--- R/hydroState.All2St.NHMM.R
+++ R/hydroState.All2St.NHMM.R
@@ -517,7 +517,7 @@ setMethod(f="getAIC",signature="hydroState.All2St.NHMM",definition=function(.Obj
 
 # Get best model (by mimimum AIC)
 #' @exportMethod getAIC.bestModel
-setGeneric(name="getAIC.bestModel",def=function(.Object, use.calib.reference.criteria=NA, use.sampleSize.penality=NA,min.obs.per.state=NA) {standardGeneric("getAIC.bestModel")})
+#setGeneric(name="getAIC.bestModel",def=function(.Object, use.calib.reference.criteria=NA, use.sampleSize.penality=NA,min.obs.per.state=NA) {standardGeneric("getAIC.bestModel")})
 setMethod(f="getAIC.bestModel",signature="hydroState.All2St.NHMM",definition=function(.Object, use.calib.reference.criteria=T, use.sampleSize.penality=F, min.obs.per.state=3)
 {
   # Get AIC
